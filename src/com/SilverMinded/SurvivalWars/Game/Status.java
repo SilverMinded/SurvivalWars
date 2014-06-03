8bfd08f7225d19f6df76afc592ea589741a52ca1
Testyflesty <Chris@iMac-van-Chris.local>
2014-06-03 16:58:42 +0200

    Enum Status aangemaakt

--- src/com/SilverMinded/SurvivalWars/Game/Status.java
+++ src/com/SilverMinded/SurvivalWars/Game/Status.java
@@ -1,5 +1,31 @@
 package com.SilverMinded.SurvivalWars.Game;
 
-public class Status {
+public enum Status {
+	IN_LOBBY(true), IN_GAME(false), POST_GAME(false), RESETTING(false);
+	
+	private boolean canJoin;
+	
+	private static Status currentStatus;
+	
+	Status(boolean canJoin) {
+		this.canJoin = canJoin;
+		
+	}
+	public boolean canJoin() {
+		return canJoin;
+	}
+	
+	public static void setStatus(Status status){
+		Status.currentStatus = status;
+	}
+	
+	public static boolean isStatus(Status status){
+		return Status.currentStatus == status;
+	}
+	
+	public static Status getStatus(){
+		return currentStatus;
+	}
+	
 
 }
