576b1b684fbc0e600028599a57198f750ff673df
J0QUE_ <j0que_@outlook.com>
2014-06-03 16:50:58 +0200

    Fix

--- src/com/SilverMinded/SurvivalWars/SurvivalWars.java
+++ src/com/SilverMinded/SurvivalWars/SurvivalWars.java
@@ -35,24 +35,24 @@ public class SurvivalWars extends JavaPlugin {
 	@EventHandler
 	@Override
 	public void onEnable() {
-<<<<<<< HEAD
+
 		getServer().getPluginManager().registerEvents(new JoinServerEvent(),
 				this);
-=======
+
 		new Thread(new CountDown()).start();
-		getServer().getPluginManager().registerEvents(new JoinServerEvent(), this);
->>>>>>> d25d1fb89abcf3a9db9becc6698404249292aea0
+		getServer().getPluginManager().registerEvents(new JoinServerEvent(),
+				this);
+
 		log.info(normalTitle + " Enabled");
 		getServer().getPluginCommand("quit").setExecutor(new Commands());
 	}
+
 	public static void start() {
-		
-		
+
 	}
-	
+
 	public static void stop() {
-		
-		
+
 	}
 
 }
