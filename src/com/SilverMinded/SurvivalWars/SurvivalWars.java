3b1050ea52aec3c02aadb2700f4b5a1b69216264
J0QUE_ <j0que_@outlook.com>
2014-06-03 15:21:21 +0200

    Basic Setup

---
+++ src/com/SilverMinded/SurvivalWars/SurvivalWars.java
@@ -0,0 +1,21 @@
+package com.SilverMinded.SurvivalWars;
+
+import java.io.File;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+public class SurvivalWars extends JavaPlugin {
+
+	public SurvivalWars() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SurvivalWars(JavaPluginLoader loader,
+			PluginDescriptionFile description, File dataFolder, File file) {
+		super(loader, description, dataFolder, file);
+		// TODO Auto-generated constructor stub
+	}
+
+}
