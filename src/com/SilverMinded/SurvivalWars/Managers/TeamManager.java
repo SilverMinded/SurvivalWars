6921ceaf43bf59002358e20a96e25fc8a16d8a11
Testyflesty <Chris@iMac-van-Chris.local>
2014-06-03 15:52:37 +0200

    getTeamWithLessPlayers

--- src/com/SilverMinded/SurvivalWars/Managers/TeamManager.java
+++ src/com/SilverMinded/SurvivalWars/Managers/TeamManager.java
@@ -1,5 +1,22 @@
 package com.SilverMinded.SurvivalWars.Managers;
 
-public class TeamManager {
+import com.SilverMinded.SurvivalWars.Game.Team;
+
+public class TeamManager extends Team{
+	
+	int blueplayers;
+	int redplayers;
+	
+	public static boolean getTeamWithLessPlayers;{
+		
+		if(blueplayers < redplayers){
+			//Hier voeg je de player toe aan team rood en else: aan team blue (andersom xD )
+			
+			
+			
+			
+		}
+	}
+	
 
 }
