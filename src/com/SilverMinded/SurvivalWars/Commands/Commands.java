93c6452a1b09d8269c05580e360b38efa57c2e7d
J0QUE_ <j0que_@outlook.com>
2014-06-03 17:01:30 +0200

    Status implementatie.

--- src/com/SilverMinded/SurvivalWars/Commands/Commands.java
+++ src/com/SilverMinded/SurvivalWars/Commands/Commands.java
@@ -4,6 +4,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.SilverMinded.SurvivalWars.Game.Status;
+
 public class Commands implements CommandExecutor {
 
 	@Override
@@ -12,6 +14,8 @@ public class Commands implements CommandExecutor {
 
 		if (command.getName().equalsIgnoreCase("quit")) {
 
+		}else if (command.getName().equalsIgnoreCase("reset")){
+			Status.setStatus(Status.RESETTING);
 		}
 
 		return false;
