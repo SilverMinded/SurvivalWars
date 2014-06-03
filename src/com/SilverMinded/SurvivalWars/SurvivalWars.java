0344f43c8983462eb63de167b9cae5fbbb3036e5
J0QUE_ <j0que_@outlook.com>
2014-06-03 22:23:51 +0200

    En opeens veel overzichtelijker

--- src/com/SilverMinded/SurvivalWars/SurvivalWars.java
+++ src/com/SilverMinded/SurvivalWars/SurvivalWars.java
@@ -1,13 +1,10 @@
 package com.SilverMinded.SurvivalWars;
 
-import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.JavaPluginLoader;
 
 import com.SilverMinded.SurvivalWars.Commands.Commands;
 import com.SilverMinded.SurvivalWars.Events.JoinServerEvent;
@@ -21,16 +18,6 @@ public class SurvivalWars extends JavaPlugin {
 	public static String normalTitle = "[SurvivalWars]";
 
 	public static SurvivalWars instance;
-
-	public SurvivalWars() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public SurvivalWars(JavaPluginLoader loader,
-			PluginDescriptionFile description, File dataFolder, File file) {
-		super(loader, description, dataFolder, file);
-		// TODO Auto-generated constructor stub
-	}
 
 	@EventHandler
 	@Override
