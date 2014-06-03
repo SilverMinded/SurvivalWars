110fc0f67924c460583fe0ef5c2f870095b0c8db
J0QUE_ <j0que_@outlook.com>
2014-06-03 15:26:17 +0200

    It's Alive!

--- src/com/SilverMinded/SurvivalWars/SurvivalWars.java
+++ src/com/SilverMinded/SurvivalWars/SurvivalWars.java
@@ -1,12 +1,20 @@
 package com.SilverMinded.SurvivalWars;
 
 import java.io.File;
+import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 public class SurvivalWars extends JavaPlugin {
+	
+	public static Logger log = Logger.getLogger("Minecraft");
+	public static String title = ChatColor.WHITE + "[" + ChatColor.DARK_GREEN
+			+ "SurvivalWars" + ChatColor.WHITE + "]";
+	public static String normalTitle = "[SurvivalWars]";
 
 	public SurvivalWars() {
 		// TODO Auto-generated constructor stub
@@ -16,6 +24,13 @@ public class SurvivalWars extends JavaPlugin {
 			PluginDescriptionFile description, File dataFolder, File file) {
 		super(loader, description, dataFolder, file);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@EventHandler
+	@Override
+	public void onEnable() {
+		log.info(normalTitle + " Enabled");
+		super.onEnable();
 	}
 
 }
