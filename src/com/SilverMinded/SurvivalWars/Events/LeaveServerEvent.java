f09780ac641269c8f17ee03225a4bef166278678
J0QUE_ <j0que_@outlook.com>
2014-06-15 13:54:51 +0200

    Leaving the server is also important...

--- src/com/SilverMinded/SurvivalWars/Events/LeaveServerEvent.java
+++ src/com/SilverMinded/SurvivalWars/Events/LeaveServerEvent.java
@@ -4,9 +4,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import com.SilverMinded.SurvivalWars.SurvivalWars;
+import com.SilverMinded.SurvivalWars.Managers.ChatManager;
+
 public class LeaveServerEvent implements Listener {
 	@EventHandler
 	public void playerQuit(PlayerQuitEvent e) {
+		
+		e.setQuitMessage("");
+		
+		ChatManager.broadcast("Player "+e.getPlayer().getDisplayName()+" has left the game.");
+		
+		SurvivalWars.log.info(e.getPlayer().getDisplayName()+" Left.");
 
 	}
 	
