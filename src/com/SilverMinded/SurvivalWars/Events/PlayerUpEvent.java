c59bbadfd6c7ab80f42ca49746f52e572f969b92
J0QUE_ <j0que_@outlook.com>
2014-06-15 12:26:56 +0200

    Why say that you levelled up more than one level, when that isn't possible without commands...

--- src/com/SilverMinded/SurvivalWars/Events/PlayerUpEvent.java
+++ src/com/SilverMinded/SurvivalWars/Events/PlayerUpEvent.java
@@ -11,9 +11,13 @@ public class PlayerUpEvent implements Listener {
 	@EventHandler
 	public void levelUp(PlayerLevelChangeEvent e) {
 
-		e.getPlayer().sendMessage(SurvivalWars.title + " You levelled up "+ (e.getNewLevel()-e.getOldLevel()) +" level(s). (New level "+e.getNewLevel()+")");
-		
-		if(e.getNewLevel()==30) ChatManager.broadcast("Player "+e.getPlayer().getPlayerListName()+" has reached level 30!");
+		e.getPlayer().sendMessage(
+				SurvivalWars.title + " You levelled up!" + " (New level "
+						+ e.getNewLevel() + ")");
+
+		if (e.getNewLevel() == 30)
+			ChatManager.broadcast("Player " + e.getPlayer().getPlayerListName()
+					+ " has reached level 30!");
 
 	}
 }
