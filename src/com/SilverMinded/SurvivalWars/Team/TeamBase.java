d9a7ef29dc5ab6ec1fe9a9ea126644055dfc2355
J0QUE_ <j0que_@outlook.com>
2014-07-02 16:27:27 +0200

    Right, this should work.

--- src/com/SilverMinded/SurvivalWars/Team/TeamBase.java
+++ src/com/SilverMinded/SurvivalWars/Team/TeamBase.java
@@ -5,31 +5,38 @@ import org.bukkit.entity.Player;
 
 public abstract class TeamBase implements ITeam {
 
-    private static int bluePlayers;
-    private static TeamBlue blue = new TeamBlue();
+    private static final TeamBlue blue = new TeamBlue();
+    private static int bluePlayers=0;
+    private static int redPlayers=0;
 
-    private static int redPlayers;
+    private static int yellowPlayers=0;
 
-    private static int yellowPlayers;
-
-    private static int purplePlayers;
+    private static int purplePlayers=0;
 
 
     public static Team getTeamWithLessPlayers() throws NullPointerException {
 
-        if (bluePlayers < (redPlayers & yellowPlayers & purplePlayers)) {
+        if(bluePlayers==0){
+            SurvivalWars.log.info("blue");
+            bluePlayers++;
+            return Team.blue;
+        }
+
+        if ((bluePlayers < (redPlayers & yellowPlayers & purplePlayers))) {
             //Hier voeg je de player toe aan team rood en else: aan team blue (andersom xD )
             SurvivalWars.log.info("blue");
+            bluePlayers++;
             return Team.blue;
 
-
         } else if (redPlayers < (bluePlayers & yellowPlayers & purplePlayers)) {
-
+            redPlayers++;
             return Team.red;
 
         } else if (yellowPlayers < (bluePlayers & redPlayers & purplePlayers)) {
+            yellowPlayers++;
             return Team.yellow;
         } else if (purplePlayers < (bluePlayers & redPlayers & yellowPlayers)) {
+            purplePlayers++;
             return Team.purple;
         }
         throw new NullPointerException("Please add a Team that exists");
@@ -38,6 +45,7 @@ public abstract class TeamBase implements ITeam {
     public static boolean joinTeam(Team team, Player player) {
         if (team == Team.blue) {
             blue.addToTeam(player);
+            return true;
         }
 
 
